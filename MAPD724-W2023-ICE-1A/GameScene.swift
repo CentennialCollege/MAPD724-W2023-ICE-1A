0e4076231f68bc1bc4484efb03e408753eba2e88
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-01-16 20:56:28 -0500

    added Player to the Scene

--- MAPD724-W2023-ICE-1A/GameScene.swift
+++ MAPD724-W2023-ICE-1A/GameScene.swift
@@ -8,6 +8,7 @@ class GameScene: SKScene
     // instance variables
     var ocean1 : Ocean?
     var ocean2 : Ocean?
+    var player : Player?
     
     override func sceneDidLoad()
     {
@@ -24,22 +25,28 @@ class GameScene: SKScene
         ocean2?.Start()
         ocean2?.position.y = -773
         addChild(ocean2!)
+        
+        // add the player to the scene
+        player = Player()
+        player?.Start()
+        addChild(player!)
     }
     
     
     func touchDown(atPoint pos : CGPoint)
     {
-        
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
     }
     
     func touchMoved(toPoint pos : CGPoint)
     {
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
         
     }
     
     func touchUp(atPoint pos : CGPoint)
     {
-        
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: -495))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -67,5 +74,6 @@ class GameScene: SKScene
     {
         ocean1?.Update()
         ocean2?.Update()
+        player?.Update()
     }
 }
